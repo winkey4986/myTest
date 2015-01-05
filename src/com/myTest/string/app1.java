3b9f7cdb70af14064fa85cf503054929c3be2bdf
winkey <winkey4986@sina.com>
2015-01-05 17:37:28 +0800

    follow

--- src/com/myTest/string/app1.java
+++ src/com/myTest/string/app1.java
@@ -3,7 +3,7 @@ package com.myTest.string;
 public class app1 {
 
 	public static void main(String[] args) {
-		String a = "1,2,3,4,4,5,6,7,8,9";
+		String a = "1,2,3,4,4,5,6,7,8";
 		//String[] readWriteType = a.split(",");
 		System.out.println(a.substring(a.length()-1,a.length()));
 	}
