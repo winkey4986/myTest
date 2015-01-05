3cdc346d0ccdc5570d9420fa21d16fae8567bb53
winkey <winkey4986@sina.com>
2015-01-05 17:37:10 +0800

    producer and consumer

---
+++ src/com/myTest/producersAndConsumers/TestArr.java
@@ -0,0 +1,11 @@
+package com.myTest.producersAndConsumers;
+
+/**
+ * Created by winkey on 15-1-5.
+ */
+public class TestArr {
+    public static void main(String[] args) {
+       /* String[] a = new String[6];
+        System.out.println("a.length:"+a.length);*/
+    }
+}
